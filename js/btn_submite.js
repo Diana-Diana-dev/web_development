c4c155be4d9faa30fe7a96343a810b00960cad37
Diana-Diana-dev <dianaargunina@gmail.com>
2021-05-27 19:07:40 +0300

    fix pop up

--- js/btn_submite.js
+++ js/btn_submite.js
@@ -3,25 +3,33 @@ let btn = document.querySelector('#SubscribeBtn');
   let cloceBtn = document.querySelector('#closeBtn');
   let modal_close = document.querySelector('#modal-close');
   let input = document.querySelector('#input');
+  let form = document.querySelector('#form');
+
+  form.onsubmit = function(evt){
+    if(input.value == ''){
+      evt.preventDefaut();
+    }else {modal.style.display = 'flex';
+    }
+  }
 
 
-  btn.addEventListener('click', function() {
-  	if (input.value == '') {
-  		return false;
-  	}else {modal.style.display = 'flex';
-  	}
-  })
+  // btn.addEventListener('click', function() {
+  // 	if (input.value == '') {
+  // 		return false;
+  // 	}else {modal.style.display = 'flex';
+  // 	}
+  // });
 
   cloceBtn.addEventListener('click', function() { 
   	modal.style.display = 'none';
-  })
+  });
 
   modal_close.addEventListener('click', function() { 
   	modal.style.display = 'none';
-  })
+  });
 
   window.addEventListener('click', function(e) { 
   	if(e.target == modal){
   	modal.style.display = 'none';
   	}
-  })
+  });
